c058669cfabd4193aded3f9363d01f3cc414f691
unlimitechcloud <65127352+unlimitechcloud@users.noreply.github.com>
2025-06-10 20:41:06 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
-#
+package main
+
+import (
+	"terraform-provider-remote/remote"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: remote.Provider,
+	})
+}
